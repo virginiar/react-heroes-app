c6ba6c9e496f7a04e7821db556fff873717e8b4b
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-06 18:03:41 +0100

    Añadiendo los componentes CustomMenu y NavigationMenu

---
+++ src/components/custom/CustomMenu.tsx
@@ -0,0 +1,42 @@
+import { Link, useLocation } from "react-router";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "../ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/* Home */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(isActive("/") && "bg-slate-200", "p-2 rounded-md")}
+          >
+            <Link to="/">Inicio</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        {/* Search */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(isActive("/search") && "bg-slate-200", "p-2 rounded-md")}
+          >
+            <Link to="/search">Buscar superhéroes</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,9 +1,11 @@
+import { CustomMenu } from "@/components/custom/CustomMenu";
 import { Outlet } from "react-router";
 
 export const HeroesLayout = () => {
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-purple-50">
       <div className="max-w-7xl mx-auto p-6">
+        <CustomMenu />
         <Outlet />
       </div>
     </div>
