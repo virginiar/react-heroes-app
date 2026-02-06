ac05ab87bf934099881c87938fc20170eed2d3e7
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-06 14:55:23 +0100

    Separando componentes HeroGrid y HeroGridCard y actualizando progress componente con activeColor

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,29 +1,32 @@
-import * as React from "react"
-import { Progress as ProgressPrimitive } from "radix-ui"
+import * as React from "react";
+import { Progress as ProgressPrimitive } from "radix-ui";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Progress({
   className,
   value,
+  activeColor = 'bg-primary',
   ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+}: React.ComponentProps<typeof ProgressPrimitive.Root> & {
+  activeColor?: string;
+}) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
         "bg-primary/20 relative h-2 w-full overflow-hidden rounded-full",
-        className
+        className,
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
+        className={cn(activeColor, "h-full w-full flex-1 transition-all")}
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
-  )
+  );
 }
 
-export { Progress }
+export { Progress };
