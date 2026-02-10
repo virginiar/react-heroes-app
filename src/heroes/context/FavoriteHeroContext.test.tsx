847f9b0d6536f433cbf8c11b278dbc7b67cd41b8
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-10 12:15:43 +0100

    Añadiendo pruebas de los métodos del contexto en FavoriteHeroContext

--- src/heroes/context/FavoriteHeroContext.test.tsx
+++ src/heroes/context/FavoriteHeroContext.test.tsx
@@ -1,11 +1,16 @@
 import { use } from "react";
-import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-
+import { beforeEach, describe, expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
 import {
   FavoriteHeroContext,
   FavoriteHeroProvider,
 } from "./FavoriteHeroContext";
+import type { Hero } from "../interfaces/hero.interface";
+
+const mockHero = {
+  id: "1",
+  name: "batman",
+} as Hero;
 
 const TestComponent = () => {
   const { favoriteCount, favorites, isFavorite, toggleFavorite } =
@@ -22,6 +27,14 @@ const TestComponent = () => {
           </div>
         ))}
       </div>
+
+      <button
+        data-testid="toggle-favorite"
+        onClick={() => toggleFavorite(mockHero)}
+      >
+        Toggle Favorite
+      </button>
+      <div data-testid="is-favorite">{isFavorite(mockHero).toString()}</div>
     </div>
   );
 };
@@ -35,10 +48,44 @@ const renderContextTest = () => {
 };
 
 describe("FavoriteHeroContext", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
   test("should initialize with default values", () => {
     renderContextTest();
 
     expect(screen.getByTestId("favorite-count").textContent).toBe("0");
     expect(screen.getByTestId("favorite-list").children.length).toBe(0);
+  });
+
+  test("should add hero to favorites when toggleFavorite is called with new Hero", () => {
+    renderContextTest();
+    const button = screen.getByTestId("toggle-favorite");
+
+    fireEvent.click(button);
+
+    expect(screen.getByTestId("favorite-count").textContent).toBe("1");
+    expect(screen.getByTestId("is-favorite").textContent).toBe("true");
+    expect(screen.getByTestId("hero-1").textContent).toBe("batman");
+    expect(localStorage.getItem("favorites")).toBe(
+      '[{"id":"1","name":"batman"}]',
+    );
+  });
+
+  test("should remove hero from favorites when toggleFavorite is called", () => {
+    localStorage.setItem("favorites", JSON.stringify([mockHero]));
+
+    renderContextTest();
+    expect(screen.getByTestId("favorite-count").textContent).toBe("1");
+    expect(screen.getByTestId("is-favorite").textContent).toBe("true");
+    expect(screen.getByTestId("hero-1").textContent).toBe("batman");
+
+    const button = screen.getByTestId("toggle-favorite");
+    fireEvent.click(button);
+
+    expect(screen.getByTestId("favorite-count").textContent).toBe("0");
+    expect(screen.getByTestId("is-favorite").textContent).toBe("false");
+    expect(screen.queryByTestId("hero-1")).toBeNull();
   });
 });
