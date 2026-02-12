b9e6c70e555eb90b2f3a65a0977183fd9a665496
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-12 10:03:53 +0100

    Pruebas adicionales en CustomPagination

--- src/components/custom/CustomPagination.test.tsx
+++ src/components/custom/CustomPagination.test.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter } from "react-router";
 import type { PropsWithChildren } from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test, vi } from "vitest";
 import { CustomPagination } from "./CustomPagination";
 
@@ -10,8 +10,16 @@ vi.mock("../ui/button", () => ({
   ),
 }));
 
-const renderWithRouter = (component: React.ReactElement) => {
+/* const renderWithRouter = (component: React.ReactElement) => {
   return render(<MemoryRouter>{component}</MemoryRouter>);
+}; */
+const renderWithRouter = (
+  component: React.ReactElement,
+  initialEntries?: string[],
+) => {
+  return render(
+    <MemoryRouter initialEntries={initialEntries}>{component}</MemoryRouter>,
+  );
 };
 
 describe("CustomPagination", () => {
@@ -27,5 +35,41 @@ describe("CustomPagination", () => {
     expect(screen.getByText("3")).toBeDefined();
     expect(screen.getByText("4")).toBeDefined();
     expect(screen.getByText("5")).toBeDefined();
+  });
+
+  test("should disabled previous button when page is 1", () => {
+    renderWithRouter(<CustomPagination totalPages={5} />);
+    const previousButton = screen.getByText("Anteriores");
+    expect(previousButton.getAttributeNames()).toContain("disabled");
+  });
+
+  test("should disabled next button when we are in the last page", () => {
+    renderWithRouter(<CustomPagination totalPages={5} />, ["/?page=5"]);
+    const nextButton = screen.getByText("Siguientes");
+
+    expect(nextButton.getAttributeNames()).toContain("disabled");
+  });
+
+  test("should disabled button 3 when we are in page 3", () => {
+    renderWithRouter(<CustomPagination totalPages={10} />, ["/?page=3"]);
+    const button2 = screen.getByText("2");
+    const button3 = screen.getByText("3");
+
+    expect(button2.getAttribute("variant")).toBe("outline");
+    expect(button3.getAttribute("variant")).toBe("default");
+  });
+
+  test("should change page when click on number button", () => {
+    renderWithRouter(<CustomPagination totalPages={5} />, ["/?page=3"]);
+
+    const button2 = screen.getByText("2");
+    const button3 = screen.getByText("3");
+    expect(button2.getAttribute("variant")).toBe("outline");
+    expect(button3.getAttribute("variant")).toBe("default");
+
+    fireEvent.click(button2);
+
+    expect(button2.getAttribute("variant")).toBe("default");
+    expect(button3.getAttribute("variant")).toBe("outline");
   });
 });
